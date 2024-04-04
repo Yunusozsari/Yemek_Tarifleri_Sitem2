e0fc5809c9d8b7a067dbb202c23f1e12e2e1cdae
Yunusozsari <131616917+Yunusozsari@users.noreply.github.com>
2024-04-04 12:01:26 +0300

    6.video5.video#

---
+++ Yemek_Tarifleri_Sitem/sqlsinif.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Yemek_Tarifleri_Sitem
+{
+    public class sqlsınıf
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=LAPTOP-DO1DBGUP\SQLEXPRESS;Initial Catalog=Dbo_yemektarif;Integrated Security=True;Encrypt=False;");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
