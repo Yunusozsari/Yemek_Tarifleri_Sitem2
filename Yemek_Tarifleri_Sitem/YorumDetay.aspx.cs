f1d5a7ce8a109825af36cbcd978e28b28fc381b9
Yunusozsari <131616917+Yunusozsari@users.noreply.github.com>
2024-04-13 16:01:13 +0300

    41.VİDEO

--- Yemek_Tarifleri_Sitem/YorumDetay.aspx.cs
+++ Yemek_Tarifleri_Sitem/YorumDetay.aspx.cs
@@ -16,23 +16,32 @@ namespace Yemek_Tarifleri_Sitem
         {
             id = Request.QueryString["Yorumid"];
 
-            SqlCommand komut = new SqlCommand("Select YorumAdSoyad,YorumMail,Yorumİcerik,YemekAd From Tbl_Yorumlar inner join Tbl_Yemekler on Tbl_Yorumlar.yemekid=tbl_yemekler.yemekid where yorumid=@p1", bgl.baglanti());
-            komut.Parameters.AddWithValue("@p1", id);
-            SqlDataReader dr = komut.ExecuteReader();
-            while (dr.Read())
+            if (Page.IsPostBack == false)
             {
-                TxtAdSoyad.Text = dr[0].ToString();
-                TxtMail.Text = dr[1].ToString();
-                Txticerik.Text = dr[2].ToString();
-                TxtYemek.Text = dr[3].ToString();
+                SqlCommand komut = new SqlCommand("Select YorumAdSoyad,YorumMail,Yorumİcerik,YemekAd From Tbl_Yorumlar inner join Tbl_Yemekler on Tbl_Yorumlar.yemekid=tbl_yemekler.yemekid where yorumid=@p1", bgl.baglanti());
+                komut.Parameters.AddWithValue("@p1", id);
+                SqlDataReader dr = komut.ExecuteReader();
+                while (dr.Read())
+                {
+                    TxtAdSoyad.Text = dr[0].ToString();
+                    TxtMail.Text = dr[1].ToString();
+                    Txticerik.Text = dr[2].ToString();
+                    TxtYemek.Text = dr[3].ToString();
+
+                }
+                bgl.baglanti().Close();
 
             }
-            bgl.baglanti().Close();
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            SqlCommand komut = new SqlCommand("Update Tbl_Yorumlar set Yorumicerik=@p1,yorumonay=@p2 where yorumid=@p3", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", Txticerik.Text);
+            komut.Parameters.AddWithValue("p2", "True");
+            komut.Parameters.AddWithValue("p3", id);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
         }
     }
 }
